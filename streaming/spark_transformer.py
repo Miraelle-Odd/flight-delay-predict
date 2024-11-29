c7c6d6311e403b01f162ee7c20009b0cd725ce4a
NA <19521191@gm.uit.edu.vn>
2024-11-29 13:52:08 +0700

    improve code

---
+++ streaming/spark_transformer.py
@@ -0,0 +1,3 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+import pyspark.sql.functions as func
--- streaming/spark_transfromer.py
+++
@@ -1,3 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-import pyspark.sql.functions as func
