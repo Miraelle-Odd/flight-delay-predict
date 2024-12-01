3a24ed15e9c667db21cb6a3544e74b124bf5807f
NA <19521191@gm.uit.edu.vn>
2024-12-01 18:18:38 +0700

    1201: Spark Cassandra connect

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Link Training Data: https://drive.google.com/uc?id=1rSmpN_7dqb08XBCILyAmxXLjprwB
 # install dependencies:
 pip install kafka-python pandas
 pip install git+https://github.com/dpkp/kafka-python.git
+
+# note
+install python 3.11.8 for the best compapility with pyspark-cassandra
+set PYSPARK_PYTHON with the path to python.exe in env variables
--- streaming/spark_manager.py
+++ streaming/spark_manager.py
@@ -1,32 +1,106 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 import pyspark.sql.functions as func
-from spark_transfromer import *
+from spark_transformer import *
+import os
+import pandas as pd
+
+# File paths
+spk_manager_dir = os.path.dirname(__file__)
+DATA_FILE_PATH = os.path.join(spk_manager_dir, "..", "Final_Data/kg-fightdelay-dataset", "T_ONTIME_REPORTING_2021_M1.csv")
+SPARK_TEMP_FOLDER = os.path.join(spk_manager_dir, 'spark-temp')
+
+CASS_HOST_IP = '127.0.0.1'
+CASS_PORT = '9042'
+CASS_USERNAME = 'cassandra'
+CASS_PASSWORD = 'cassandra'
 
 def createSparkSession(appName):
     return SparkSession.builder.appName(appName).getOrCreate()
 
-def readCsvData(sparkSession, filePath='Final_Data/kg-fightdelay-dataset/T_ONTIME_REPORTING_2021_M1.csv'):
+def readCsvData(sparkSession, filePath=DATA_FILE_PATH):
     return sparkSession.read.format('csv').\
         option('inferSchema','true').\
         option('header', 'true').\
         option('path', filePath).\
         load()
 
-def createCassSession(appName, hostIp="127.0.0.1" ):
-    return SparkSession.builder.\
-        appName(appName).\
-        config("spark.cassandra.connection.host", hostIp).\
-        getOrCreate()
+def createCassSession(appName):
+    try:
+        print('--- Connecting to Cassandra ---')
+        cassSession = SparkSession.builder.\
+            appName(appName)\
+            .config("spark.cassandra.connection.host", CASS_HOST_IP)\
+            .config("spark.cassandra.connection.port", CASS_PORT)\
+            .config("spark.cassandra.auth.username", CASS_USERNAME)\
+            .config("spark.cassandra.auth.password", CASS_PASSWORD)\
+            .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.0.0") \
+            .config("spark.local.dir", SPARK_TEMP_FOLDER) \
+            .getOrCreate()
+        print('--- Connection to Cassandra created successfully! ---')
+        return cassSession
+    except Exception as e:
+        print('-- Fail to create Cassandra connection with error: --', e)
 
 def writeToCass(dataset, table, keyspace='testframe'):
-    dataset.write\
-        .format("org.apache.spark.sql.cassandra")\
-        .mode('append')\
-        .options(table=table, keyspace=keyspace)\
-        .save()
+    try:
+        print(f'-- Start writing to table {table} in Cassandra --')
+        dataset.write\
+            .format("org.apache.spark.sql.cassandra")\
+            .mode('append')\
+            .options(table=table, keyspace=keyspace)\
+            .save()
+        print(f'-- Completed Writing to table {table} in Cassandra --')
+    except Exception as e:
+        print('-- Writing to Cassandra fail with error: --', e)
 
-sparkSession = createSparkSession("FlightData")
-data = readCsvData(sparkSession)
+def readFromCass(cassSession, table, keyspace='testframe'):
+    try:
+        print(f'-- Start reading table {table} from Cassandra --')
+        df = cassSession.read \
+            .format("org.apache.spark.sql.cassandra") \
+            .options(table=table, keyspace=keyspace) \
+            .load()
+        print(f'--- Reading table {table} from Cassandra successfully! ---')
+        return df
+    except Exception as e:
+        print('-- Reading from Cassandra fail with error: --', e)
+        return
+    
+def testable():
+    print('--Test Starting--')
+    cassSession = createCassSession("FlightData")    
+    
+    data = [(6, 28, "IO1"), (7, 32, "NA1")]
+    columns = ["id", "age", "name"]
+    print('--We will be writing these following data to cassandra and show it--')
+    print(data,columns)
+    
+    new_df = cassSession.createDataFrame(data, columns)
+    writeToCass(new_df, 'testable')
+    
+    df = readFromCass(cassSession, 'testable')
+    df.printSchema()
+    df.show()
+    
+    cassSession.stop()
+    print('--Test Finish--')
+    
+def spark_manager():
+    cassSession = createCassSession("FlightData")
+    data = readCsvData(cassSession)
+    
+    data.printSchema()
+    data.limit(2).show()    
+    
+    # data.createOrReplaceTempView('flights_temp')
+    # print(cassSession.catalog.listTables())
+    cassSession.stop()
 
-data.printSchema()
+if __name__ == "__main__":
+    try:
+        # testable()     
+        spark_manager()
+                
+    except Exception as e:
+        print('-- General exception caught: --', e)
